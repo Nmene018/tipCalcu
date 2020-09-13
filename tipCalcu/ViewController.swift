70532bbdfda80e50f17c2a05b02666a5f1caf298
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-09-13 01:20:48 -0400

    Initial Commit

---
+++ tipCalcu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalcu
+//
+//  Created by Natalie Meneses on 9/13/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
