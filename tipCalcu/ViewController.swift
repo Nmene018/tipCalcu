4cf600b3fe8116ff0d7030ffd72037fc157d8225
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-09-14 02:14:57 -0400

    With Tip Percentage Feature

--- tipCalcu/ViewController.swift
+++ tipCalcu/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var eachPersonSlider: UISlider!
     @IBOutlet weak var eachPersonLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
     @IBOutlet weak var eachPersonAmount: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,20 +34,26 @@ class ViewController: UIViewController {
         //Get the bill amount
         let bill = Double(billField.text!) ?? 0
         //Calculate the tip and total
-        let tipPercentages = [0.15,0.18,0.2]
-        
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        let tipPercent = tipSlider.value
+        let tipAmount = bill * Double(tipPercent) / 100
+        tipLabel.text = String(format: "$%.1f", tipPercent)
+        tipAmountLabel.text =  String(format: "$%.2f", tipAmount)
+
         //Update the tip and total labels
-        tipLabel.text = String(format: "%.2f", tip)
-            totalLabel.text = String(format: "$%.2f", total)
-        
+        let total = bill + tipAmount
+        let finTotal = round(total*100)/100
+        totalLabel.text=String(format: "$%.2f", finTotal)
+
+        //Get the slider and total amount for each person
         let numberOfPeople = Int(eachPersonSlider.value)
         eachPersonLabel.text="Split: \(numberOfPeople)"
         let eachTotal=total/Double(numberOfPeople)
         eachPersonAmount.text = String(format: "$%.2f", eachTotal)
+
         
     }
+
+    
     
 }
 
