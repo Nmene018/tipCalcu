4f229de774f463ca7e5fb623af774406509a94a3
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-09-13 23:43:03 -0400

    Split Feature

--- tipCalcu/ViewController.swift
+++ tipCalcu/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var eachPersonSlider: UISlider!
+    @IBOutlet weak var eachPersonLabel: UILabel!
+    @IBOutlet weak var eachPersonAmount: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,18 +26,24 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    
     @IBAction func calculateTip(_ sender: Any) {
         
         //Get the bill amount
         let bill = Double(billField.text!) ?? 0
         //Calculate the tip and total
-        let tipPercentages = [0.1,0.18,0.2]
+        let tipPercentages = [0.15,0.18,0.2]
         
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         //Update the tip and total labels
-        tipLabel.text = String(format: "$%.2f", tip)
+        tipLabel.text = String(format: "%.2f", tip)
             totalLabel.text = String(format: "$%.2f", total)
+        
+        let numberOfPeople = Int(eachPersonSlider.value)
+        eachPersonLabel.text="Split: \(numberOfPeople)"
+        let eachTotal=total/Double(numberOfPeople)
+        eachPersonAmount.text = String(format: "$%.2f", eachTotal)
         
     }
     
